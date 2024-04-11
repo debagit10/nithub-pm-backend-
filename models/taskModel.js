98abc7e3893d53e6d70344f9df479be19a448712
Josiah Elias <josiahelias11@gmail.com>
2024-04-12 00:51:01 +0100

    feat: add a task model

---
+++ models/taskModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const taskModel = new mongoose.Schema({
+  about: { type: String, required: true },
+  deadline: { type: String, required: true },
+  links: { type: Array },
+  files: { type: Array },
+  project_id: { type: String, required: true },
+  assignee_id: { type: String, required: true },
+});
+
+const Task = mongoose.model("Task", taskModel);
+
+module.exports = Task;
