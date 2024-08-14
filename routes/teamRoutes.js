72529ed93888062bd1ec3cb9c9e496f5f85b87f8
Josiah Elias <josiahelias11@gmail.com>
2024-08-14 12:18:41 +0100

    fix: removed authUser middleware from the get team detail route

--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -3,6 +3,6 @@ const { addTeam, getTeam } = require("../controllers/teamControllers");
 const { authUser } = require("../config/authUser");
 
 router.post("/add", authUser, addTeam);
-router.get("/getDetail", authUser, getTeam);
+router.get("/getDetail", getTeam);
 
 module.exports = router;
